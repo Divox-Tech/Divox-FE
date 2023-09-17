4a12804c6cf2af738bed893d0e696cae44b65675
Divox-Technologies <nfo@divox.io>
2023-09-17 22:55:25 +0300

    Integration with sendGrid

--- src/app/core/divox-header/divox-header.component.ts
+++ src/app/core/divox-header/divox-header.component.ts
@@ -1,27 +1,28 @@
 import {Component, EventEmitter, HostListener, Output} from '@angular/core';
-import {AppFacade} from "../../services/app.facade";
-
 @Component({
   selector: 'divox-header',
-  templateUrl: './divox-header.component.html',
-  styleUrls: ['./divox-header.component.scss']
+templateUrl: './divox-header.component.html',
+styleUrls: ['./divox-header.component.scss']
 })
+
 export class DivoxHeaderComponent {
-
+  isMenuChanged = false;
   constructor(
-    // private appFacade: AppFacade
   ) {}
-
-  @Output("linkClicked") linkClicked = new EventEmitter<string>();
-
+@Output("linkClicked") linkClicked = new EventEmitter<string>();
   selectedLanguage: string = "en";
-
   setLinkClicked(link: string){
     this.linkClicked.emit(link);
   }
-
+  menuOnClick(){
+    this.isMenuChanged = !this.isMenuChanged;
+  }
   changeLanguage(lang: string){
     this.selectedLanguage = lang;
   }
-
 }
+
+
+
+
+
